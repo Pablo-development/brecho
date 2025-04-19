aee7cda1f8810202fdba104f1f3ada72ad64731a
Pablo <pablo.mikolaiczyki@gmail.com>
2025-04-19 17:27:08 -0300

    created Category entity

---
+++ src/main/java/br/org/catolicasc/model/Category.java
@@ -0,0 +1,30 @@
+package br.org.catolicasc.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> product;
+}
