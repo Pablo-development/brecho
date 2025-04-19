f7a10c3f3ba01451080f13127f135cf24b604d08
Pablo <pablo.mikolaiczyki@gmail.com>
2025-04-19 01:31:39 -0300

    rename class

--- src/main/java/br/org/catolicasc/BrechoApplication.java
+++ src/main/java/br/org/catolicasc/BrechoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatautoApplication {
+public class BrechoApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(CatautoApplication.class, args);
+		SpringApplication.run(BrechoApplication.class, args);
 	}
 }
