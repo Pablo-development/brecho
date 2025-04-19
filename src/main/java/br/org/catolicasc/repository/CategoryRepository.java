46e2865e3c0c43ae512fe73b7d08a586fa86545d
Pablo <pablo.mikolaiczyki@gmail.com>
2025-04-19 17:27:22 -0300

    created Category repository

---
+++ src/main/java/br/org/catolicasc/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package br.org.catolicasc.repository;
+
+import br.org.catolicasc.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
