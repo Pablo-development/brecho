ee1830dd8ae9e4b66a79539c44e9141074ae284c
Pablo <pablo.mikolaiczyki@gmail.com>
2025-04-20 23:58:23 -0300

    Created method findcategorybyid

--- src/main/java/br/org/catolicasc/repository/CategoryRepository.java
+++ src/main/java/br/org/catolicasc/repository/CategoryRepository.java
@@ -4,4 +4,5 @@ import br.org.catolicasc.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    Category findCategoryById(Long id);
 }
